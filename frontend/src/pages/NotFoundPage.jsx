4a78cd44b06e20d6b52db59f8d295b0f35a3ab41
cjj702 <cjj702m@gmail.com>
2026-08-05 16:52:15 +0900

    fix: 에러페이지 버튼 동작 수정

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import EmptyState from '../components/ui/EmptyState'
 import './NotFoundPage.css'
 
@@ -9,7 +8,7 @@ export default function NotFoundPage() {
         icon="documents"
         title="페이지를 찾을 수 없습니다"
         description="주소가 변경되었거나 존재하지 않는 페이지입니다. 홈에서 다시 시작해 주세요."
-        action={<Link className="btn btn--primary" to="/">홈으로 이동</Link>}
+        action={{ to: '/', label: '홈으로 이동' }}
       />
     </main>
   )
